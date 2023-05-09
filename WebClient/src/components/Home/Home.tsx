cc24ff1debe535885fe27b5fe0c0b6e739923095
pankazakos <pankazakos01@gmail.com>
2023-05-09 17:25:55 +0300

    add Footer.tsx

--- WebClient/src/components/Home/Home.tsx
+++ WebClient/src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Shared/Header";
+import Footer from "../Shared/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div>Home</div>
+      <Footer />
     </>
   );
 }
