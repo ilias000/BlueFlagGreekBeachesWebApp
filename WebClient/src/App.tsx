0b5b161f4fa2906a85f215b062672e8d5231c549
pankazakos <pankazakos01@gmail.com>
2023-05-09 15:59:16 +0300

    basic routes and components

--- WebClient/src/App.tsx
+++ WebClient/src/App.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Admin from "./components/Admin/Admin";
+import Home from "./components/Home/Home";
+import Notifications from "./components/Notifications/Notifications";
+import NotFound from "./components/Shared/NotFound";
+
+const AppRouter = createBrowserRouter([
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/notifications",
+    element: <Notifications />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 export default function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <RouterProvider router={AppRouter} />
+    </>
+  );
 }
