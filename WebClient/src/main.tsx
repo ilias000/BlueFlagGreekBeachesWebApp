7be5ec1aac7e5163098954a39a9c15bffc72dd52
pankazakos <pankazakos01@gmail.com>
2023-05-09 15:09:19 +0300

    debug mode

--- WebClient/src/main.tsx
+++ WebClient/src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./css/index.css";
 
+const debug = true; // if true, components render twice to find errors
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    {debug ? (
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    ) : (
+      <>
+        <App />
+      </>
+    )}
+  </>
 );
