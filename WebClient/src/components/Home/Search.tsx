b43e8b7bac5a7666ba6ec900d2d309ecca7f7092
pankazakos <pankazakos01@gmail.com>
2023-06-21 01:30:23 +0300

    fix margin

--- WebClient/src/components/Home/Search.tsx
+++ WebClient/src/components/Home/Search.tsx
@@ -10,6 +10,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import AuthContext from "../Shared/Auth";
 import Header from "../Shared/Header";
 import Footer from "../Shared/Footer";
+import { Box } from "@mui/material";
 
 const categories = [{ label: "Αθηναίων" }, { label: "Ζωγράφου" }];
 
@@ -24,50 +25,50 @@ export default function Search() {
   return (
     <>
       <Header />
-      <form onSubmit={handleSubmit}>
-        <Grid container columnGap={5} mt={3} mb={3}>
-          <Grid>
-            <Autocomplete
-              multiple
-              filterSelectedOptions
-              disablePortal
-              options={categories}
-              sx={{ width: 300, mt: 2 }}
-              renderInput={(params) => <TextField {...params} label="Κατηγορίες" />}
-            />
+      <Box sx={{ width: "90%", margin: "0 auto", marginTop: "4rem", marginBottom: "1.5rem" }}>
+        <form onSubmit={handleSubmit}>
+          <Grid container columnGap={5} justifyContent="flex-start">
+            <Grid>
+              <Autocomplete
+                multiple
+                filterSelectedOptions
+                disablePortal
+                options={categories}
+                sx={{ width: 300, mt: 2 }}
+                renderInput={(params) => <TextField {...params} label="Κατηγορίες" />}
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                sx={{ width: 300, mt: 2 }}
+                label="λέξεις-κλειδιά"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid>
+              <Button
+                type="submit"
+                variant="text"
+                sx={{ BackgroundColor: "var(--primary-color)", mt: 2, height: AuthData.isLoggedIn ? "2.5rem" : "3.5rem" }}
+              >
+                ΑΝΑΖΗΤΗΣΗ
+              </Button>
+              {AuthData.isLoggedIn && (
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} />} label={<div
+                    style={{
+                      fontSize: 14
+                    }}
+                  >
+                    Αποθήκευση
+                  </div>}
+                  />
+                </FormGroup>
+              )}
+            </Grid>
           </Grid>
-          <Grid>
-            <TextField
-              sx={{ width: 300, mt: 2 }}
-              label="λέξεις-κλειδιά"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid>
-            <Button
-              type="submit"
-              variant="text"
-              sx={{ BackgroundColor: "var(--primary-color)", mt: 2, height: AuthData.isLoggedIn ? "2.5rem" : "3.5rem" }}
-            >
-              ΑΝΑΖΗΤΗΣΗ
-            </Button>
-            {AuthData.isLoggedIn && (
-              <FormGroup>
-                <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 18 } }} />} label={<div
-                  style={{
-                    fontSize: 14
-                  }}
-                >
-                  Αποθήκευση
-                </div>} sx={{
-
-                }} />
-              </FormGroup>
-            )}
-          </Grid>
-        </Grid>
-      </form>
-
+        </form>
+      </Box>
       <Map />
       <Footer />
     </>
