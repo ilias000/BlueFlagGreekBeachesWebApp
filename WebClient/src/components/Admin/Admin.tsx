f39a3cd41cc23a9dbbbcffd968d36a06df4ff5f7
SteliosOik <steoik@hotmail.com>
2023-07-07 04:12:31 +0300

    Created Admin page mockup

--- WebClient/src/components/Admin/Admin.tsx
+++ WebClient/src/components/Admin/Admin.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Box, Tab, Tabs, Typography } from '@mui/material';
+
+import Header from "../Secondary/Header";
+import Footer from "../Secondary/Footer";
+import UserList from "./UserList";
+import DataUpload from "./DataUpload";
 
 export default function Admin() {
-  return <div>Admin</div>;
+
+  const [adminTab, setadminTab] = useState('usersTab');
+  const handleAdminTab = (event, tab) => {
+    setadminTab(tab);
+  };
+
+  return (
+    <>
+      <Header />
+      
+      <Box sx={{ width: "60%", margin: "0 auto", marginTop: "5.5rem"}}>
+        <Tabs value={adminTab} onChange={handleAdminTab} centered 
+              sx={{
+                '& .MuiTab-root': {
+                  margin: '0 10%',
+                },
+                marginBottom: '1.5rem'
+              }}>
+          <Tab value='usersTab' label=" ΛΙΣΤΑ ΧΡΗΣΤΩΝ" />
+          <Tab value='dataTab' label="ΔΕΔΟΜΕΝΑ" />
+        </Tabs>
+
+        <Box style={{ height: '100vh'}}>
+          {adminTab === 'usersTab' && <UserList />}
+          {adminTab === 'dataTab' && <DataUpload />}
+        </Box>
+      </Box>
+
+      <Footer />
+    </>
+  );
+
 }
