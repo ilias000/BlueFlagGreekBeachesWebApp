45a3d207097670f052e094a0436de64f0650e0bd
ilias000 <il.piotopoulos@gmail.com>
2023-07-07 14:45:18 +0300

    #12 User functionality.

--- API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/entity/User.java
+++ API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/entity/User.java
@@ -1,11 +1,15 @@
 package com.BlueFlagGreekBeaches.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,11 +21,20 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID) // Instructs that a UUID for the entity should be generated automatically for us by the persistence provider.
+    @Column(name = "id", nullable = false)
     private UUID id;
 
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "is_admin", nullable = false)
+    private Boolean isAdmin = false;
+
+    @ManyToMany(mappedBy = "users")
+    private List<SaveSearch> saveSearches = new ArrayList<>();
 
     public User(String email, String password)
     {
--- API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/repository/UserRepository.java
+++ API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/repository/UserRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID>
 {
+    boolean existsUserByEmail(String email);
 }
