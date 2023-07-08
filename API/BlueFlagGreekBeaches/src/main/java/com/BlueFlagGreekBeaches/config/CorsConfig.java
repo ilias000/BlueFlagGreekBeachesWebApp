3f9f08f59a4523fa0823e0b192b6ad79ea793ff1
kbenetou <kbenetou@verallis.com>
2023-07-08 14:29:32 +0300

    removeValue

--- API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/config/CorsConfig.java
+++ API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.BlueFlagGreekBeaches.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,15 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-    @Value("${allowed.origin}")
-    private String allowedOrigin;
     @Bean
     public WebMvcConfigurer getCorsConfiguration(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
               registry.addMapping("/**")
-                      .allowedOrigins(allowedOrigin)
+                      .allowedOrigins("http://localhost:5173")
                       .allowedMethods("*")
                       .allowedHeaders("*");
             }
