ede1003c611dacd4fce3e777fd56a04e14f9e7a6
ilias000 <il.piotopoulos@gmail.com>
2023-07-09 21:44:17 +0300

    Login password encryption and Token.

---
+++ API/BlueFlagGreekBeaches/src/main/java/com/BlueFlagGreekBeaches/dto/user/LoginUserDto.java
@@ -0,0 +1,5 @@
+package com.BlueFlagGreekBeaches.dto.user;
+
+public record LoginUserDto(String email, String password) // Private, final fields, getters, equals, hashCode, toString and constructor for all fields.
+{
+}
